1885e7abb7b184eeb6c2166d0201cddf780b67d1
Antoine RICHARD <antoine_510@hotmail.fr>
2022-10-19 22:05:28 +0200

    Added Serial comms using RS485

--- MPPTino.ino
+++ MPPTino.ino
@@ -7,12 +7,13 @@
 #include <ST7565.h>
 #include <LowPower.h>
 
+static constexpr const int serial_identity = 0x01;
+
 const uint8_t MCP4561address = 0x2e;
 uint8_t MCPWiper = 0;
-//const uint8_t trackingSpeed = 4u;  // Maximum change of wiper: 4 steps/s, one step is approx. 0.1A
+
 PAC1710 pac;
-typedef PAC1710::ValueReader<2, PAC1710::SS_20MV> PACReader;
-constexpr uint8_t SERIAL_QUERY_PIN = 3;
+typedef PAC1710::ValueReader<2, PAC1710::SS_80MV> PACReader;
 bool sleep = true;
 
 struct SerialData {
@@ -21,8 +22,8 @@ struct SerialData {
 	uint16_t deciwatts;
 	uint16_t joules;
 };
-SerialData sdata;
-bool sendNow = false;
+SerialData* sdata, *sdataLast;
+uint16_t global_joules = 0;
 
 void setWiper(uint8_t value) {
 	Wire.beginTransmission(MCP4561address);
@@ -40,10 +41,6 @@ void moveWiper(bool decrement) {
 		MCPWiper += 1 - 2 * decrement;
 }
 
-void serial_query_ISR() {
-	if(sleep) return;
-	sendNow = true;
-}
 
 void setup() {
 	ADCSRA &= ~(1 << ADEN); // Disable ADC
@@ -53,7 +50,10 @@ void setup() {
 	setWiper(MCPWiper);
 	pac.SetSamplingTimesMs(20, 80);
 	pac.SetAveraging(PAC1710::AVG_NONE, PAC1710::AVG_8);
-	pac.SetSenseScale(PAC1710::SS_20MV);
+	pac.SetSenseScale(PAC1710::SS_80MV);
+
+	sdata = new SerialData{};
+	sdataLast = new SerialData{};
 
 	ST7565::begin(0x03);
 	ST7565::clear();
@@ -65,8 +65,6 @@ void setup() {
 	ST7565::drawchar_aligned(5, 2, 'A');
 
 	ST7565::drawchar_aligned(4, 4, 'W');
-
-	attachInterrupt(digitalPinToInterrupt(SERIAL_QUERY_PIN), &serial_query_ISR, RISING);
 }
 
 constexpr unsigned minVoltage = 30000, maxVoltage = 35000;
@@ -80,11 +78,10 @@ constexpr uint8_t power_reactivity_scaling = 32;
 
 // True for descreasing power, false for increasing power
 bool exploration_direction = false;
+uint8_t before_crash_wiper = 0;
 
-uint16_t last_power = 0;
-
-constexpr unsigned long loopTime = 500ul;
-unsigned loopActualTime;
+constexpr unsigned long loopTime = 1000ul;
+unsigned loopDuration = 0;
 byte crashCount = 0;
 constexpr byte sleepCrashCount = 5; // If we have been under the crash voltage for more than sleepCrashCount * loopTime, sleep
 
@@ -100,31 +97,34 @@ void increasePower() {
 }
 
 void loop() {
-	static unsigned long lastLoop = millis();
-	loopActualTime = millis() - lastLoop;
-	lastLoop = millis();
+	unsigned long loopStartTime = millis();
 
 	pac.ReadOnce(PAC1710::READ_ALL);
-	sdata.millivolts = PACReader::GetVoltageI(pac);
-	sdata.milliamps = abs(PACReader::GetCurrentI(pac));
-	sdata.deciwatts = PACReader::GetPowerI(pac);
+	sdata->millivolts = PACReader::GetVoltageI(pac);
+	sdata->milliamps = abs(PACReader::GetCurrentI(pac));
+	sdata->deciwatts = PACReader::GetPowerI(pac);
 
-	sdata.joules += (uint32_t)sdata.deciwatts * loopActualTime / 10000;
+	global_joules += (uint32_t)sdata->deciwatts * loopDuration / 10000;
 
-	if(sdata.millivolts < minVoltage) {	// Under-production
+	if(sdataLast->millivolts - sdata->millivolts > 5000) {	// Crashed 5V in 1s
+		before_crash_wiper = MCPWiper;
+		setWiper((uint32_t)sdata->millivolts * MCPWiper / sdataLast->millivolts);
+	} else if(sdata->millivolts < minVoltage) {	// Under-production
 		decreasePower();
 		exploration_direction = true;
-	} else if(sdata.millivolts > maxVoltage) {	// Over-production/Ouput limited
+	} else if(before_crash_wiper > 0) {
+		setWiper(before_crash_wiper - 1 - before_crash_wiper / power_reactivity_scaling);
+		before_crash_wiper = 0;
+	} else if(sdata->millivolts > maxVoltage) {	// Over-production/Ouput limited
 		increasePower();
 		exploration_direction = false;
 	} else {
 		// Random walk within [minV, maxV]
-		if(last_power > sdata.deciwatts) {
+		if(sdataLast->deciwatts > sdata->deciwatts) {
 			exploration_direction = !exploration_direction;
 		}
 		moveWiper(exploration_direction);
 	}
-	last_power = sdata.deciwatts;
 
 	if(!MCPWiper) {	// Handle going to sleep
 		if(crashCount > sleepCrashCount) {
@@ -143,30 +143,30 @@ void loop() {
 		crashCount = 0;
 	}
 
-	if(sendNow) {
+	if(Serial.read() == serial_identity) {
+		sdata.joules = global_joules;
 		Serial.write((unsigned char*)(&sdata), sizeof(sdata));
-		sdata.joules = 0;
-		sendNow = false;
+		global_joules = 0;
 	}
 
-	byte tensOfVolts = sdata.millivolts / 10000;
+	byte tensOfVolts = sdata->millivolts / 10000;
 	ST7565::drawchar_aligned(0, 0, tensOfVolts ? tensOfVolts + 48 : 0);
-	ST7565::drawchar_aligned(1, 0, (sdata.millivolts / 1000) % 10 + 48);
-	ST7565::drawchar_aligned(3, 0, (sdata.millivolts / 100) % 10 + 48);
+	ST7565::drawchar_aligned(1, 0, (sdata->millivolts / 1000) % 10 + 48);
+	ST7565::drawchar_aligned(3, 0, (sdata->millivolts / 100) % 10 + 48);
 
-	byte tensOfAmps = sdata.milliamps / 10000;
+	byte tensOfAmps = sdata->milliamps / 10000;
 	ST7565::drawchar_aligned(0, 2, tensOfAmps ? tensOfAmps + 48 : 0);
-	ST7565::drawchar_aligned(1, 2, (sdata.milliamps / 1000) % 10 + 48);
-	ST7565::drawchar_aligned(3, 2, (sdata.milliamps / 100) % 10 + 48);
-	ST7565::drawchar_aligned(4, 2, (sdata.milliamps / 10) % 10 + 48);
+	ST7565::drawchar_aligned(1, 2, (sdata->milliamps / 1000) % 10 + 48);
+	ST7565::drawchar_aligned(3, 2, (sdata->milliamps / 100) % 10 + 48);
+	ST7565::drawchar_aligned(4, 2, (sdata->milliamps / 10) % 10 + 48);
 
-	byte thousandsOfWatts = sdata.deciwatts / 10000;
-	byte hundredsOfWatts = (sdata.deciwatts / 1000) % 10;
-	byte tensOfWatts = (sdata.deciwatts / 100) % 10;
+	byte thousandsOfWatts = sdata->deciwatts / 10000;
+	byte hundredsOfWatts = (sdata->deciwatts / 1000) % 10;
+	byte tensOfWatts = (sdata->deciwatts / 100) % 10;
 	ST7565::drawchar_aligned(0, 4, thousandsOfWatts ? thousandsOfWatts + 48 : 0);
 	ST7565::drawchar_aligned(1, 4, (hundredsOfWatts || thousandsOfWatts) ? hundredsOfWatts + 48 : 0);
 	ST7565::drawchar_aligned(2, 4, (tensOfWatts || hundredsOfWatts || thousandsOfWatts) ? tensOfWatts + 48 : 0);
-	ST7565::drawchar_aligned(3, 4, (sdata.deciwatts / 10) % 10 + 48);
+	ST7565::drawchar_aligned(3, 4, (sdata->deciwatts / 10) % 10 + 48);
 
 	byte hundredsOfMCP = MCPWiper / 100;
 	byte tensOfMCP = (MCPWiper / 10) % 10;
@@ -176,6 +176,12 @@ void loop() {
 
 	ST7565::display();
 
+	SerialData* t = sdata;
+	sdata = sdataLast;
+	sdataLast = t;
+
 	if(sleep) LowPower.powerDown(SLEEP_8S, ADC_ON, BOD_ON);
 	else delay(loopTime);
+
+	loopDuration = millis() - loopStartTime;
 }
