2df368625f54ff1bcdd46b42273d882d3d91a737
Antoine Richard <antoine_510@hotmail.fr>
2023-05-21 12:44:07 +0200

    Stateless MPP algorithm, improved joule counting precision, MPP now working with target volts

--- MPPTino.ino
+++ MPPTino.ino
@@ -13,8 +13,9 @@ enum SerialState : uint8_t {
 	MAGIC1, MAGIC2, MAGIC3, MAGIC4, IDENTITY, COMMAND
 } serial_state;
 enum CommandID : uint8_t {
-	READ_ALL = 0x01,
-	SET_MPP_VOLTAGE_MV = 0x02,	// Manually set MPP voltage in millivolts
+	NONE,
+	READ_ALL,
+	SET_MPP_VOLTAGE_DV,	// Manually set MPP voltage in decivolts
 };
 
 struct SerialData {
@@ -24,10 +25,13 @@ struct SerialData {
 	uint16_t pout_dw;
 	uint16_t eout_j;
 };
-SerialData* sdata, *sdataLast;
-uint16_t global_joules = 0;
+SerialData sdata;
+uint32_t eout_mj = 0;
 
 MCP4716 dac;
+void SetMPPVoltage(uint16_t mppv_dv) {
+	dac.SetValue((mppv_dv * 17) / 10 + 196);
+}
 
 void setup() {
 	Serial.begin(9600);
@@ -36,9 +40,6 @@ void setup() {
 	pinMode(PIN_VIN, INPUT);
 	pinMode(PIN_VOUT, INPUT);
 	pinMode(PIN_IOUT, INPUT);
-
-	sdata = new SerialData{};
-	sdataLast = new SerialData{};
 
 	ST7565::begin(0x03);
 	ST7565::clear();
@@ -54,22 +55,22 @@ void setup() {
 
 	ST7565::drawchar_aligned(4, 6, 'W');
 
-  dac.SetValue(700);
+	SetMPPVoltage(320);
 }
-
-uint16_t mpp_voltage_mv = 33000;
 
 constexpr unsigned long stateUpdatePeriod = 1000ul;
 
 void runCommand(CommandID command) {
+	uint16_t mppv_dv;
 	switch(command) {
 	case READ_ALL:
-		sdata->eout_j = global_joules;
-		Serial.write((byte*)(sdata), sizeof(SerialData));
-		global_joules = 0;
+		sdata.eout_j = eout_mj / 1000;
+		eout_mj = 0;
+		Serial.write((uint8_t*)(&sdata), sizeof(SerialData));
 		break;
-	case SET_MPP_VOLTAGE_MV:
-		Serial.readBytes((uint8_t*)&mpp_voltage_mv, sizeof(mpp_voltage_mv));
+	case SET_MPP_VOLTAGE_DV:
+		Serial.readBytes((uint8_t*)&mppv_dv, sizeof(mppv_dv));
+		SetMPPVoltage(mppv_dv);
 		break;
 	}
 	serial_state = MAGIC1;	// Done with command, reset serial state
@@ -77,41 +78,37 @@ void runCommand(CommandID command) {
 
 unsigned long lastStateUpdate = 0;
 void updateState() {
-	SerialData* t = sdata;
-	sdata = sdataLast;
-	sdataLast = t;
+	sdata.vin_cv = (uint16_t)analogRead(PIN_VIN) * 45 / 10;
+	sdata.vout_dv = (uint32_t)analogRead(PIN_VOUT) * 10264 / 10000;
+	sdata.iout_ca = (uint16_t)(analogRead(PIN_IOUT) + 2) * 14 / 9;
+	sdata.pout_dw = (uint32_t)sdata.vout_dv * sdata.iout_ca / 100;
 
-	sdata->vin_cv = (uint16_t)analogRead(PIN_VIN) * 45 / 10;
-	sdata->vout_dv = (uint32_t)analogRead(PIN_VOUT) * 10264 / 10000;
-	sdata->iout_ca = (uint16_t)(analogRead(PIN_IOUT) + 2) * 14 / 9;
-	sdata->pout_dw = (uint32_t)sdata->vout_dv * sdata->iout_ca / 100;
-
-	global_joules += (uint32_t)sdata->pout_dw * (uint32_t)(millis() - lastStateUpdate) / 10000;
+	eout_mj += (uint32_t)sdata.pout_dw * (millis() - lastStateUpdate) / 10;
 	lastStateUpdate = millis();
 
-	byte tensOfVolts = sdata->vin_cv / 1000;
+	byte tensOfVolts = sdata.vin_cv / 1000;
 	ST7565::drawchar_aligned(0, 0, tensOfVolts ? tensOfVolts + 48 : 0);
-	ST7565::drawchar_aligned(1, 0, (sdata->vin_cv / 100) % 10 + 48);
-	ST7565::drawchar_aligned(3, 0, (sdata->vin_cv / 10) % 10 + 48);
+	ST7565::drawchar_aligned(1, 0, (sdata.vin_cv / 100) % 10 + 48);
+	ST7565::drawchar_aligned(3, 0, (sdata.vin_cv / 10) % 10 + 48);
 
-	tensOfVolts = sdata->vout_dv / 100;
+	tensOfVolts = sdata.vout_dv / 100;
 	ST7565::drawchar_aligned(0, 2, tensOfVolts ? tensOfVolts + 48 : 0);
-	ST7565::drawchar_aligned(1, 2, (sdata->vout_dv / 10) % 10 + 48);
-	ST7565::drawchar_aligned(3, 2, sdata->vout_dv % 10 + 48);
+	ST7565::drawchar_aligned(1, 2, (sdata.vout_dv / 10) % 10 + 48);
+	ST7565::drawchar_aligned(3, 2, sdata.vout_dv % 10 + 48);
 
-	byte tensOfAmps = sdata->iout_ca / 1000;
+	byte tensOfAmps = sdata.iout_ca / 1000;
 	ST7565::drawchar_aligned(0, 4, tensOfAmps ? tensOfAmps + 48 : 0);
-	ST7565::drawchar_aligned(1, 4, (sdata->iout_ca / 100) % 10 + 48);
-	ST7565::drawchar_aligned(3, 4, (sdata->iout_ca / 10) % 10 + 48);
-	ST7565::drawchar_aligned(4, 4, sdata->iout_ca % 10 + 48);
+	ST7565::drawchar_aligned(1, 4, (sdata.iout_ca / 100) % 10 + 48);
+	ST7565::drawchar_aligned(3, 4, (sdata.iout_ca / 10) % 10 + 48);
+	ST7565::drawchar_aligned(4, 4, sdata.iout_ca % 10 + 48);
 
-	byte thousandsOfWatts = sdata->pout_dw / 10000;
-	byte hundredsOfWatts = (sdata->pout_dw / 1000) % 10;
-	byte tensOfWatts = (sdata->pout_dw / 100) % 10;
+	byte thousandsOfWatts = sdata.pout_dw / 10000;
+	byte hundredsOfWatts = (sdata.pout_dw / 1000) % 10;
+	byte tensOfWatts = (sdata.pout_dw / 100) % 10;
 	ST7565::drawchar_aligned(0, 6, thousandsOfWatts ? thousandsOfWatts + 48 : 0);
 	ST7565::drawchar_aligned(1, 6, (hundredsOfWatts || thousandsOfWatts) ? hundredsOfWatts + 48 : 0);
 	ST7565::drawchar_aligned(2, 6, (tensOfWatts || hundredsOfWatts || thousandsOfWatts) ? tensOfWatts + 48 : 0);
-	ST7565::drawchar_aligned(3, 6, (sdata->pout_dw / 10) % 10 + 48);
+	ST7565::drawchar_aligned(3, 6, (sdata.pout_dw / 10) % 10 + 48);
 
 	ST7565::display();
 }
