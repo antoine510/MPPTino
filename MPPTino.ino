ff7173c5a08bfd1f9c44b6e6406b81a710ae2716
Antoine RICHARD <antoine_510@hotmail.fr>
2023-06-03 20:43:01 +0200

    Added temperature sensing using DS18B20 sensor

--- MPPTino.ino
+++ MPPTino.ino
@@ -1,5 +1,6 @@
 #include <ST7565.h>
 #include <MCP4716.h>
+#include <OneWire.h>	// For DS18B20
 
 enum Pins {
 	PIN_VIN = A1,
@@ -24,6 +25,7 @@ struct SerialData {
 	uint16_t iout_ca;
 	uint16_t pout_dw;
 	uint16_t eout_j;
+	int8_t temp_c;
 };
 SerialData sdata;
 uint32_t eout_mj = 0;
@@ -31,6 +33,21 @@ uint32_t eout_mj = 0;
 MCP4716 dac;
 void SetMPPVoltage(uint16_t mppv_dv) {
 	dac.SetValue((mppv_dv * 17) / 10 + 196);
+}
+
+int8_t GetTemperature() {
+	static OneWire tsens(9);
+
+	tsens.reset();
+	tsens.skip();
+	tsens.write(0xbe);
+	byte l = tsens.read(), h = tsens.read();
+	int8_t res = (int8_t)((l >> 4) | (h << 4));
+
+	tsens.reset();
+	tsens.skip();
+	tsens.write(0x44);	// Start next measurement
+	return res;
 }
 
 void setup() {
@@ -45,10 +62,11 @@ void setup() {
 	ST7565::clear();
 
 	ST7565::drawchar_aligned(2, 0, '.');
-	ST7565::drawchar_aligned(4, 0, 'V');
 
-	ST7565::drawchar_aligned(2, 2, '.');
-	ST7565::drawchar_aligned(4, 2, 'V');
+	ST7565::drawchar_aligned(7, 0, '.');
+	ST7565::drawchar_aligned(9, 0, 'V');
+
+	ST7565::drawchar_aligned(3, 2, 'C');
 
 	ST7565::drawchar_aligned(2, 4, '.');
 	ST7565::drawchar_aligned(5, 4, 'A');
@@ -82,6 +100,7 @@ void updateState() {
 	sdata.vout_dv = (uint16_t)analogRead(PIN_VOUT) * 39 / 38;
 	sdata.iout_ca = (uint16_t)(analogRead(PIN_IOUT) + 2) * 14 / 9;
 	sdata.pout_dw = (uint32_t)sdata.vout_dv * sdata.iout_ca / 100;
+	sdata.temp_c = GetTemperature();
 
 	eout_mj += (uint32_t)sdata.pout_dw * (millis() - lastStateUpdate) / 10;
 	lastStateUpdate = millis();
@@ -92,9 +111,19 @@ void updateState() {
 	ST7565::drawchar_aligned(3, 0, (sdata.vin_cv / 10) % 10 + 48);
 
 	tensOfVolts = sdata.vout_dv / 100;
-	ST7565::drawchar_aligned(0, 2, tensOfVolts ? tensOfVolts + 48 : 0);
-	ST7565::drawchar_aligned(1, 2, (sdata.vout_dv / 10) % 10 + 48);
-	ST7565::drawchar_aligned(3, 2, sdata.vout_dv % 10 + 48);
+	ST7565::drawchar_aligned(5, 0, tensOfVolts ? tensOfVolts + 48 : 0);
+	ST7565::drawchar_aligned(6, 0, (sdata.vout_dv / 10) % 10 + 48);
+	ST7565::drawchar_aligned(8, 0, sdata.vout_dv % 10 + 48);
+
+	byte tempi = 0;
+	int8_t temp = sdata.temp_c;
+	if(temp < 0) {
+		ST7565::drawchar_aligned(tempi++, 2, '-');
+		temp = -temp;
+	}
+	byte tensOfDegrees = temp / 10;
+	ST7565::drawchar_aligned(tempi++, 2, tensOfDegrees ? tensOfDegrees + 48 : 0);
+	ST7565::drawchar_aligned(tempi++, 2, temp % 10 + 48);
 
 	byte tensOfAmps = sdata.iout_ca / 1000;
 	ST7565::drawchar_aligned(0, 4, tensOfAmps ? tensOfAmps + 48 : 0);
