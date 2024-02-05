1d6240216dadbba50e35a01acd0db51759be403f
Antoine RICHARD <antoine_510@hotmail.fr>
2024-02-05 22:08:50 +0100

    Deleted temp sensor for board v2.3

--- MPPTino.ino
+++ MPPTino.ino
@@ -1,13 +1,11 @@
 #include <EEPROM.h>
 #include <ST7565.h>
 #include <MCP4716.h>
-#include <OneWire.h>	// For DS18B20
 
 enum Pins {
 	PIN_VIN = A1,
 	PIN_VOUT = A2,
 	PIN_IOUT = A3,
-  PIN_TEMPERATURE = 9,
   PIN_TXEN = 2
 };
 
@@ -15,8 +13,7 @@ enum CommandID : uint8_t {
 	NONE,
 	READ_ALL,
 	SET_MPP_MANUAL_DV,	// Manually set MPP voltage in decivolts
-  SET_MPP_AUTO,
-  SET_MAX_TEMP_C  // Set maximum allowed temperature in celcius
+  SET_MPP_AUTO
 };
 
 struct SerialData {
@@ -25,13 +22,12 @@ struct SerialData {
 	uint16_t iout_ca;
 	uint16_t pout_dw;
 	uint16_t eout_j;
-	int8_t temp_c;
 };
 SerialData sdata;
 uint32_t eout_mj = 0;
 
 MCP4716 dac;
-static constexpr const uint16_t dacMin = 672, dacMax = 876; // 28V - 40V
+static constexpr const uint16_t dacMin = 672, dacMax = 791; // 28V - 35V
 constexpr uint16_t VoltageToDAC(uint16_t mppv_dv) {
   return (mppv_dv * 17) / 10 + 196;
 }
@@ -52,23 +48,6 @@ bool NudgeMPP(bool increase) {
   else saturated = false;
   dac.SetValue(dacValue);
   return saturated;
-}
-
-int8_t maxTemperature = 80;
-
-int8_t GetTemperature() {
-	static OneWire tsens(PIN_TEMPERATURE);
-
-	tsens.reset();
-	tsens.skip();
-	tsens.write(0xbe);
-	byte l = tsens.read(), h = tsens.read();
-	int8_t res = (int8_t)((l >> 4) | (h << 4));
-
-	tsens.reset();
-	tsens.skip();
-	tsens.write(0x44);	// Start next measurement
-	return res;
 }
 
 void setup() {
@@ -110,9 +89,6 @@ void runCommand(CommandID command) {
   case SET_MPP_AUTO:
     manualMPP = 0;
     break;
-  case SET_MAX_TEMP_C:
-    maxTemperature = (int8_t)Serial.read();
-    break;
 	}
 }
 
@@ -125,12 +101,9 @@ void updateState() {
 	sdata.iout_ca = (uint16_t)(analogRead(PIN_IOUT) + 2) * 14 / 9;
   uint32_t pout_mw = (uint32_t)sdata.vout_dv * sdata.iout_ca;
 	sdata.pout_dw = pout_mw / 100;
-	sdata.temp_c = GetTemperature();
 
-  if(sdata.temp_c > maxTemperature) {
-    NudgeMPP(true);
-  } else if(manualMPP && dacValue != VoltageToDAC(manualMPP)) {
-    SetMPPVoltage(manualMPP);
+  if(manualMPP) {
+    if(dacValue != VoltageToDAC(manualMPP)) SetMPPVoltage(manualMPP);
   } else {
     if(pout_mw < lastPower) nudge = !nudge;
     bool saturated = NudgeMPP(nudge);
@@ -175,11 +148,10 @@ void setupLCD() {
 	ST7565::clear();
 
 	ST7565::drawchar_aligned(2, 0, '.');
+  ST7565::drawchar_aligned(4, 0, 'V');
 
-	ST7565::drawchar_aligned(7, 0, '.');
-	ST7565::drawchar_aligned(9, 0, 'V');
-
-	ST7565::drawchar_aligned(3, 2, 'C');
+	ST7565::drawchar_aligned(2, 2, '.');
+	ST7565::drawchar_aligned(4, 2, 'V');
 
 	ST7565::drawchar_aligned(2, 4, '.');
 	ST7565::drawchar_aligned(5, 4, 'A');
@@ -194,19 +166,9 @@ void updateLCD() {
 	ST7565::drawchar_aligned(3, 0, (sdata.vin_cv / 10) % 10 + 48);
 
 	tensOfVolts = sdata.vout_dv / 100;
-	ST7565::drawchar_aligned(5, 0, tensOfVolts ? tensOfVolts + 48 : 0);
-	ST7565::drawchar_aligned(6, 0, (sdata.vout_dv / 10) % 10 + 48);
-	ST7565::drawchar_aligned(8, 0, sdata.vout_dv % 10 + 48);
-
-	byte tempi = 0;
-	int8_t temp = sdata.temp_c;
-	if(temp < 0) {
-		ST7565::drawchar_aligned(tempi++, 2, '-');
-		temp = -temp;
-	}
-	byte tensOfDegrees = temp / 10;
-	ST7565::drawchar_aligned(tempi++, 2, tensOfDegrees ? tensOfDegrees + 48 : 0);
-	ST7565::drawchar_aligned(tempi++, 2, temp % 10 + 48);
+	ST7565::drawchar_aligned(0, 2, tensOfVolts ? tensOfVolts + 48 : 0);
+	ST7565::drawchar_aligned(1, 2, (sdata.vout_dv / 10) % 10 + 48);
+	ST7565::drawchar_aligned(3, 2, sdata.vout_dv % 10 + 48);
 
 	byte tensOfAmps = sdata.iout_ca / 1000;
 	ST7565::drawchar_aligned(0, 4, tensOfAmps ? tensOfAmps + 48 : 0);
